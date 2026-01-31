59348ebe5dea3536a6ef68888b47ad7b7a25f552
waleed666-max <waleedmaxeed63@gmail.com>
2026-01-31 16:04:40 +0500

    Refactor MongoDB connection strings in db.js
    Comment out unused MongoDB connection strings.

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 /*const mongoose = require('mongoose');
 require('dotenv').config(); 
   //for local use
-// const  = 'mongodb://127.0.0.1:27017/inotebook';
+// const  = 'mongodb:///inotebook';
 //const mongoURI = 'mongodb+srv:env';
 const mongoURI = process.env.MONGO_URI;
 const connectToMongo = () => {
